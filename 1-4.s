e9d4d97712c92939fbe8b927c8debb4f384ab629
keita <banira0428@gmail.com>
2019-11-11 14:49:38 +0900

    プログラム　修正

--- 1-4.s
+++ 1-4.s
@@ -9,32 +9,45 @@ space:
 	.text
 	.align	2
 
-test_prime:				    # int test_prime(int n = $a0)
-
-	li	$a3,2			    # forのループ数カウンタ
+test_prime:				    
+	li	$a3,2			    
 
 for:
-    slt	$v0,$a3,$a1	        # if !(i < n)
-	beq	$v0,$zero,return1	#   return 1
+    slt	$v0,$a3,$a1	        
+	beq	$v0,$zero,return1	
 
-    div	$a1,$a3			    # $a0 = 
-	mfhi	$v0			    # $v0 = n % i
-	beq	$v0,$zero,return0	# if ($v0 == 0) return 0
+    div	$a1,$a3			    
+	mfhi	$v0			    
+	beq	$v0,$zero,return0	
 
-    addi	$a3, $a3, 1		# $a1 = $a1 + 0
+    addi	$a3, $a3, 1		
     j   for
 
-while:
-	slt	$v0,$a2,$a0		    # if !(i < n)
-	beq	$v0,$zero,return1	#   return 1
+return0:
+	li	$v0,0
+	j	$ra
 
-    move $s3, $ra
+return1:
+	li	$v0,1
+	j	$ra
+
+main:
+    move    $s0, $ra
+    li      $s1, 1
+    la		$a0, 100 
+    li      $a1, 2      
+    li      $a2, 0      
+    j       while
+
+while:
+	slt	$v0,$a2,$a0		    
+	beq	$v0,$zero,output	
+
     jal test_prime
-    move $ra, $s3
     beq $v0,$s1,match
 
-    addi	$a1, $a1, 1		# $a1 = $a1 + 1
-	j	while			    # goto while
+    addi	$a1, $a1, 1		
+	j	while			    
 
 match: 
     move    $s2, $a0
@@ -44,29 +57,14 @@ match:
     jal     print_string
     move    $a0, $s2
 
-    addi	$a1, $a1, 1			# $a1 = $a1 + 0
-    addi	$a2, $a2, 1			# $a1 = $a1 + 0
+    addi	$a1, $a1, 1			
+    addi	$a2, $a2, 1			
     j       while
 
-main:
-    move    $s0, $ra
-    li      $s1, 1
-    la		$a0, 100 
-    li      $a1, 2      # whileのループ数カウンタ
-    li      $a2, 0      # match数
-    jal     while
+output:
     la      $a0, newline
     jal     print_string
-    j	$s0				# jump to $s0
-
-
-return0:
-	li	$v0,0
-	j	$ra
-
-return1:
-	li	$v0,1
-	j	$ra
+    j	    $s0				
 
 print_string:
     li      $v0, 4
--- 1-5.s
+++ 1-5.s
@@ -14,10 +14,11 @@ primes:
 	.text
 	.align	2
 
-test_prime:				    
+test_prime:	
+
 	li	$a3, 2			    # forのループ数カウンタ
 
-for:
+Lfor:
     slt	$v0,$a3,$a1	        
 	beq	$v0,$zero,return1	
 
@@ -26,31 +27,15 @@ for:
 	beq	$v0,$zero,return0	
 
     addi	$a3, $a3, 1
-    j   for
+    j   Lfor	
 
-while:
-	slt	$v0,$a2,$a0		    
-	beq	$v0,$zero,return1	
+return0:
+	li	$v0,0
+	j	$ra
 
-    move $s3, $ra
-    jal test_prime
-    move $ra, $s3
-    beq $v0,$s1,match
-
-    addi	$a1, $a1, 1		
-	j	while			    
-
-match: 
-
-    move    $s3, $ra
-    move    $a3, $a2
-    jal     get_address   
-    move    $ra, $s3
-    sw      $a1, 0($v0)
-    
-    addi	$a1, $a1, 1			
-    addi	$a2, $a2, 1			
-    j       while
+return1:
+	li	$v0,1
+	j	$ra	    
 
 main:
     move    $s0, $ra
@@ -59,15 +44,32 @@ main:
     li      $a1, 2          # whileのループ数カウンタ
     li      $a2, 0          # match数
     la      $t1, primes     # 配列の先頭アドレス
-    jal     while
-    j		find			
+    j       while
+
+while:
+	slt	$v0,$a2,$a0		    
+	beq	$v0,$zero,find	
+
+    jal test_prime
+    beq $v0,$s1,match
+
+    addi	$a1, $a1, 1		
+	j	while	
+
+match: 
+    move    $a3, $a2
+    jal     get_address   
+    sw      $a1, 0($v0)
+    addi	$a1, $a1, 1			
+    addi	$a2, $a2, 1			
+    j       while
     
 find:     
     la      $a0, input
     jal     print_string
     jal     read_int
     move    $a3, $v0
-    addi    $a3, $a3, -1   # $a3 を一つ減算
+    addi    $a3, $a3, -1 
     jal		get_address	
     lw		$a0, 0($v0) 
     jal     print_int
@@ -76,19 +78,11 @@ find:
     j       find
 
 get_address:
-    addu    $a3, $a3, $a3  # $a0 (match) を倍に
-    addu    $a3, $a3, $a3  # $a0 を更に倍に ($a0 = match * 4)
-    addu    $a3, $t1, $a3  # $a0 = $t1 + $a0
+    addu    $a3, $a3, $a3  # $a3 (match) を倍に
+    addu    $a3, $a3, $a3  # $a3 を更に倍に ($a3 = match * 4)
+    addu    $a3, $t1, $a3  # $a3 = $t1 + $a3
     move    $v0, $a3
     j		$ra				# jump to $ra
-    
-return0:
-	li	$v0,0
-	j	$ra
-
-return1:
-	li	$v0,1
-	j	$ra
 
 print_string:
     li      $v0, 4
