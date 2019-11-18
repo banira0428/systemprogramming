b2940b8adf849676118b5b3461e8b2105f6d68ee
keita <banira0428@gmail.com>
2019-11-18 13:53:46 +0900

    軽微な修正

--- 1-1.s
+++ 1-1.s
@@ -13,11 +13,11 @@ main:
         .align 2  
 putc:
         lb      $a0, 0($a1)             
-        lw      $t0, 0xffff0008         # $t0 = *(0xffff0008)
-        li      $t1, 1                  # $t1 = 1
-        and     $t0, $t0, $t1           # $t0 &= $t1
-        beqz    $t0, putc               # if ($t0 == 0) goto putc
-        sw      $a0, 0xffff000c         # *(0xffff000c) = $a0
+        lw      $t0, 0xffff0008         
+        li      $t1, 1                  
+        and     $t0, $t0, $t1           
+        beqz    $t0, putc               
+        sw      $a0, 0xffff000c         
         addi    $a1, $a1, 1
         bnez    $a0, putc
-        j       $ra                     # return
+        j       $ra                     
