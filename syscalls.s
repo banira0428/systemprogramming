05b7011f49590e13305b472e1ffce8136049830b
keita <banira0428@gmail.com>
2019-12-09 14:21:03 +0900

    a

--- syscalls.s
+++ syscalls.s
@@ -22,3 +22,31 @@ print_string:
  lw      $ra,20($sp)
  addu    $sp,$sp,24
  j       $ra 
+
+read_int:
+ subu    $sp,$sp,24
+ sw      $ra,20($sp)
+
+ li      $v0, 5
+ syscall
+
+ lw      $ra,20($sp)
+ addu    $sp,$sp,24
+ j       $ra 
+
+read_string:
+ subu    $sp,$sp,24
+ sw      $ra,20($sp)
+
+ li      $v0, 8
+ syscall
+
+ lw      $ra,20($sp)
+ addu    $sp,$sp,24
+ j       $ra
+
+exit:
+ li      $v0, 10
+ syscall
+
+ 
